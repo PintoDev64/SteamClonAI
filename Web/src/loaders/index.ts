fed79de938d2dad244ddcb6c247bfca067ccb16c
Joan Sebastian Cardozo Salinas <84690368+PintoGamer64@users.noreply.github.com>
2024-07-29 22:24:58 -0500

    Add Env Variables

--- Web/src/loaders/index.ts
+++ Web/src/loaders/index.ts
@@ -1,10 +1,11 @@
+import { URL_API } from "contants";
 import { Params } from "react-router-dom";
 
 type LoaderType = { request: Request, params: Params }
 export function GameLoader({ params }: LoaderType) {
-    return fetch(`${process.env.URL_API}/api/v1/game?idGame=${params.IdGame}`)
+    return fetch(`${URL_API}/api/v1/game?idGame=${params.IdGame}`)
 }
 
 export function StoreLoader() {
-    return fetch(`${process.env.URL_API}/api/v1/store`)
+    return fetch(`${URL_API}/api/v1/store`)
 }
