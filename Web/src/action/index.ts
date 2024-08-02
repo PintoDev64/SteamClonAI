f9b29d3c2a007fdecb58c22de99aaab57551e038
Joan Sebastian Cardozo Salinas <84690368+PintoGamer64@users.noreply.github.com>
2024-08-01 22:38:41 -0500

    Type Non Used

--- Web/src/action/index.ts
+++ Web/src/action/index.ts
@@ -1,7 +1,5 @@
-import { Params, redirect } from "react-router-dom";
+import { redirect } from "react-router-dom";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-type LoaderType = { request: Request, params: Params }
 export async function LoginForm() {
     return redirect("/")
 }
